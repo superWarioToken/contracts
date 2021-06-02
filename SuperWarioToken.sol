7a96573bc960e0802f52ba1e4f5063adaa88add7
superWarioToken <83981228+superWarioToken@users.noreply.github.com>
2021-06-03 01:35:19 +0400

    Update and rename SuperWario.sol to SuperWarioToken.sol

--- SuperWarioToken.sol
+++ SuperWarioToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.6.6;
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./libs/BEP20.sol";
 
-contract SuperWario is BEP20 {
+contract SuperWarioToken is BEP20 {
     constructor() BEP20("SuperWario", "WARIO") public {
         _mint(msg.sender, 10e32);
     }
